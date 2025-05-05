1fdb5c3b3a937427507153d118e8c96ef1a412e9
abdul-codes <abdulsalamkinsanya2@gmai.com>
2025-05-05 22:15:28 +0100

    Intial setup complete

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello from TypeScript Express!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
